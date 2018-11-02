7b63d80607ca01b67c4bd7a7d22e1a8648312ec8
Mitchell Mortensen <mitchneutron@gmail.com>
2018-11-02 02:48:11 +0000

    Server works moderately.

--- clickerGame/app.js
+++ clickerGame/app.js
@@ -3,73 +3,48 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
-const favicon = require('express-favicon');
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var app = express();
+var bodyParser = require("body-parser");
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-app.use(favicon(__dirname + '/public/images/favicon_cookie.png'));
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors()) //allow all cors requests. 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //var app = window.angular.module('app', []); //this line doesn't work in this file
 //app.controller('clickerCtrl', clickerCtrl); //neither does this line
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+
+var teamData = { red: 0, blue: 0, yellow: 0 };
+
+app.post('/updateTeamData', function(req, res) {
+  console.log("Incoming post for update team data...")
+  var data = "";
+  req.on('data', function(chunk) { data += chunk })
+  req.on('end', function() {
+    req.rawBody = data;
+    req.jsonBody = JSON.parse(data);
+    if(data.indexOf("red")!=-1){
+      teamData.red++
+    }
+    
+    if(data.indexOf("blue")!=-1){
+      teamData.blue++
+    }
+    if(data.indexOf("yellow")!=-1){
+      teamData.yellow++
+    }
+    console.log(teamData)
+    
+    res.end(JSON.stringify(teamData))
+  })
+
+
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-var teamData = {red: 0, blue: 0, yellow: 0};
-
-app.post('/update', function(req, res, next) {
-  var teamColor = req.body;
-  
-  console.log("Posting: " + teamColor);
-
-  switch (teamColor) {
-    case ('red'):
-      teamData.red++;
-      break;
-    case ('blue'):
-      teamData.blue++;
-      break;
-    case ('yellow'):
-      teamData.yellow++;
-      break;
-  }
-
-  return res.send(teamData);
-});
-app.listen(8080, function(){
-  console.log("Server listening on port 8080")
+app.listen(4200, function() {
+  console.log("Server listening on port 4200")
 })
 // The controller/functions etc are in the inner app.js file (public/javascripts/app.js) cuz that's the only way I could get it to work 
 
--- clickerGame/public/javascripts/app.js
+++ clickerGame/public/javascripts/app.js
@@ -9,19 +9,25 @@ function clickerCtrl($scope, $http) {
     console.log(">NG:CLICKERCTRL called");
 
     $scope.addPoint = function(teamColor) {
+        var toSend = {color:teamColor}
         
         $http({
-            url: 'http://54.236.42.112:8080/update',
+            url: 'http://54.236.42.112:4200/update',
             method: 'POST',
-            data: teamColor
+            data: toSend
         }).then(function(httpResponse) {
             console.log('response:', httpResponse);
         })
 
-        console.log("\t AddPoint() called, teamClr= " + teamColor);
+        console.log("\t AddPoint() called, teamClr= " + toSend);
 
 
 
         console.log("\t> Team Clicks: R=" + $scope.redClicks + ", B=" + $scope.blueClicks + ", Y=" + $scope.yellowClicks);
     };
+}
+
+function myFunc(){
+    var toSend;
+    toSend.color
 }
