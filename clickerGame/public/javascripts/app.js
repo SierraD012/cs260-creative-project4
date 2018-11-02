eeba0be0a82572b609a72892cdc01a008c272504
Mitchell Mortensen <mitchneutron@gmail.com>
2018-11-02 01:49:20 +0000

    Removed merge conflicts.

--- clickerGame/public/javascripts/app.js
+++ clickerGame/public/javascripts/app.js
@@ -2,39 +2,25 @@
 
 var app = window.angular.module('clickerApp', []);
 
-
 app.controller('clickerCtrl', clickerCtrl);
-
 
 function clickerCtrl($scope, $http) {
 
-  console.log(">NG:CLICKERCTRL called");
+    console.log(">NG:CLICKERCTRL called");
 
-   
-    
-  $scope.addPoint = function(teamColor) {
-     
-     
-     
-    $http({
-        url: 'localhost:8080/update',
-        method: 'POST',
-        data: teamColor
-    }).then(function(httpResponse){
-        console.log('response:', httpResponse);
-    })
-      
-      
-    console.log("\t AddPoint() called, teamClr= " + teamColor);
-    
+    $scope.addPoint = function(teamColor) {
+        $http({
+            url: 'localhost:8080/update',
+            method: 'POST',
+            data: teamColor
+        }).then(function(httpResponse) {
+            console.log('response:', httpResponse);
+        })
 
-    
-    console.log("\t> Team Clicks: R="+$scope.redClicks + ", B=" + $scope.blueClicks + ", Y=" + $scope.yellowClicks);
-  };
-<<<<<<< HEAD
+        console.log("\t AddPoint() called, teamClr= " + teamColor);
+
+
+
+        console.log("\t> Team Clicks: R=" + $scope.redClicks + ", B=" + $scope.blueClicks + ", Y=" + $scope.yellowClicks);
+    };
 }
-=======
-  
-}
-
->>>>>>> 203c80bb003f00cab5fbf5d2603169660349f1dd
