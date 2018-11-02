4f5c513b9f3f9a93399809fe7e9883415c9cf5ff
Sierra Davis <sd.birdlover@gmail.com>
2018-11-02 02:21:03 +0000

    added code to change bg color but not sure if it works yet

--- clickerGame/public/javascripts/app.js
+++ clickerGame/public/javascripts/app.js
@@ -5,8 +5,11 @@ var app = window.angular.module('clickerApp', []);
 app.controller('clickerCtrl', clickerCtrl);
 
 function clickerCtrl($scope, $http) {
-
     console.log(">NG:CLICKERCTRL called");
+
+    $scope.redPts = 0;
+    $scope.bluePts = 0;
+    $scope.yellowPts = 0;
 
     $scope.addPoint = function(teamColor) {
         
@@ -16,12 +19,27 @@ function clickerCtrl($scope, $http) {
             data: teamColor
         }).then(function(httpResponse) {
             console.log('response:', httpResponse);
-        })
+            
+                $scope.redPts = httpResponse.red;
+                $scope.bluePts = httpResponse.blue;
+                $scope.yellowPts = httpResponse.yellow;
+  
+            //change the bg color here 
+            var winningPts = Math.max($scope.redPts, $scope.bluePts, $scope.yellowPts);
+            if (winningPts == $scope.redPts){
+              //change bg to red
+              $("mainBody").css("background-color").innerHTML("#7c0000");
+            } else if (winningPts == $scope.bluePts) {
+              //change bg to blue
+              $("mainBody").css("background-color").innerHTML("#003b9b");
+            } else {
+              //change bg to yellow
+              $("mainBody").css("background-color").innerHTML("#917d00");
+            }
+            
+        });
 
         console.log("\t AddPoint() called, teamClr= " + teamColor);
-
-
-
         console.log("\t> Team Clicks: R=" + $scope.redClicks + ", B=" + $scope.blueClicks + ", Y=" + $scope.yellowClicks);
     };
 }
